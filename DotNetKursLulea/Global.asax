39bc5c8c90d5b135b712a4d7f0e2051e88230927
millom <mikael.lundstrom@studentconsulting.com>
2016-02-10 14:04:28 +0100

    Created

---
+++ DotNetKursLulea/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetKursLulea.MvcApplication" Language="C#" %>
---
+++ DotNetKursLulea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetKursLulea.Startup))]
+namespace DotNetKursLulea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
